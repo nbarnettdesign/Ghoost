62ab91dc569517990d32f96fe87ac5fab79179eb
nbarnettdesign <nbarnett.design@gmail.com>
2017-07-18 02:31:42 +1000

    First Backup of Greybox with Triggers

---
+++ Ghost/Assets/Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+	public GameObject text1;
+	public GameObject text2;
+	public GameObject text3;
+	public GameObject text4;
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			text1.gameObject.SetActive (true);
+			text2.gameObject.SetActive (false);
+			text3.gameObject.SetActive (false);
+			text4.gameObject.SetActive (false);
+		}
+	}
+
+}
